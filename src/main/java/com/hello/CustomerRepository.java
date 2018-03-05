8e7a5ae42b3a6d38ac375af04bce39f12ac2f088
Sandhya <sandhyaj224@gmail.com>
2018-03-05 12:06:33 -0500

    First Commit

---
+++ src/main/java/com/hello/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.hello;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+
+}
